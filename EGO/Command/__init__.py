01984a5555b73246d5e55719e7e8fbd15e93c87c
Naruto777549649 <sikrar026@gmail.com>
2026-08-03 22:26:41 +0530

    Create Command/__init__.py

---
+++ EGO/Command/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+from EGO.Modules.start import *
+from EGO.Modules.upload import *
+from EGO.Modules.grab import *
+from EGO.Modules.ctime import *
+from EGO.utils.msg import *
+from EGO.Plugins.spawn import *
