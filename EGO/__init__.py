b8114ce38d8b30f04d0485fb735222ee1be2baa2
Naruto777549649 <sikrar026@gmail.com>
2026-08-03 19:22:05 +0530

    Create EGO/__init__.py

---
+++ EGO/__init__.py
@@ -0,0 +1,21 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+import logging
+
+# 🔥 Logging Setup
+logging.basicConfig(
+    format="[Naruto-Bot] %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("/app/logs.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
+
+# 🔥 Main Bot Client
+bot = Client(
+    "Waifu-Bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="EGO/Modules")
+)
+
+DEVS=[8460600506]
