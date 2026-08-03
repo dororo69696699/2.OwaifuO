8e9a990eadca452451461721cd72ac9f7bd3dcf0
Naruto777549649 <sikrar026@gmail.com>
2026-08-03 20:25:57 +0530

    Create rarity.py

---
+++ EGO/utils/rarity.py
@@ -0,0 +1,79 @@
+# RARITY_MAP
+rarity_map = {
+    1: "⚪️ Common",
+    2: "🟡 Legendary",
+    3: "💮 Special",
+    4: "🔮 Limited",
+    5: "💸 Premium",
+    6: "🌤 Summer",
+    7: "🎐 Celestial",
+    8: "❄️ Winter",
+    9: "💝 Valentine",
+    10: "🎃 Halloween",
+    11: "🎄 Christmas Special",
+    12: "🧧 Events",
+    13: "🍑 Echhi",
+    14: "🎗️ AMV",
+    15: "🌧 Rainy",
+    16: "🦠 Mythgard",
+}
+
+# RARITY_NAMES
+RARITY_NAMES = [
+    "⚪️ Common",
+    "🟡 Legendary",
+    "💮 Special",
+    "🔮 Limited",
+    "💸 Premium",
+    "🌤 Summer",
+    "🎐 Celestial",
+    "❄️ Winter",
+    "💝 Valentine",
+    "🎃 Halloween",
+    "🎄 Christmas Special",
+    "🧧 Events",
+    "🍑 Echhi",
+    "🎗️ AMV",
+    "🌧 Rainy",
+    "🦠 Mythgard",
+]
+
+# RARITY_MAP2
+rarity_map2 = {
+    "⚪️ Common": "⚪️",
+    "🟡 Legendary": "🟡",
+    "💮 Special": "💮",
+    "🔮 Limited": "🔮",
+    "💸 Premium": "💸",
+    "🌤 Summer": "🌤",
+    "🎐 Celestial": "🎐",
+    "❄️ Winter": "❄️",
+    "💝 Valentine": "💝",
+    "🎃 Halloween": "🎃",
+    "🎄 Christmas Special": "🎄",
+    "🧧 Events": "🧧",
+    "🍑 Echhi": "🍑",
+    "🎗️ AMV": "🎗️",
+    "🌧 Rainy": "🌧",
+    "🦠 Mythgard": "🦠",
+}
+
+# DEFAULT RARITY LIMIT
+DEFAULT_RARITY_LIMITS = {
+    "⚪️ Common": 100,
+    "🟡 Legendary": 50,
+    "💮 Special": 30,
+    "🔮 Limited": 25,
+    "💸 Premium": 20,
+    "🌤 Summer": 15,
+    "🎐 Celestial": 12,
+    "❄️ Winter": 10,
+    "💝 Valentine": 8,
+    "🎃 Halloween": 7,
+    "🎄 Christmas Special": 5,
+    "🧧 Events": 4,
+    "🍑 Echhi": 3,
+    "🎗️ AMV": 2,
+    "🌧 Rainy": 2,
+    "🦠 Mythgard": 1,
+}
