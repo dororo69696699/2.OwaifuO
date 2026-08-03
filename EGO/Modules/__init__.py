b11e5c839622641fcc7994fd19807a8dfce968c0
Naruto777549649 <sikrar026@gmail.com>
2026-08-03 22:12:15 +0530

    Update __init__.py

--- EGO/Modules/__init__.py
+++ EGO/Modules/__init__.py
@@ -1,6 +1,6 @@
 # __init__.py
 from EGO.Modules.start import *
-from EGO.Modules.msg import *
+from EGO.utils.msg import *
 from EGO.Modules.upload import *
 from EGO.Plugins.spawn import *
 from EGO.Modules.grab import *
