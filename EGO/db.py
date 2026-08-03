2a6583319e49cc6d11b69ff6b8fb4fe11216889a
Naruto777549649 <sikrar026@gmail.com>
2026-08-03 19:30:59 +0530

    Update db.py

--- EGO/db.py
+++ EGO/db.py
@@ -3,9 +3,21 @@ from config import MONGO_URL
 
 # MongoDB Connection
 client = AsyncIOMotorClient(MONGO_URL)
-db = client["WAIFUBOT"]
+db = client["waifucluster"]
 
-# Existing Collections
+# USERS AND GROUPS
+
 Users = db["USERS"]
 Groups = db["GROUPS"]
 Banned = ["BANNED"]
+
+# CHARACTER 
+
+collection = db['anime_characters_lol']
+user_totals_collection = db['user_totals_lmaoooo']
+user_collection = db["user_collection_lmaoooo"]
+group_user_totals_collection = db['group_user_totalsssssss']
+top_global_groups_collection = db['top_global_groups']
+pm_users = db['total_pm_users']
+discounts_collection = db['discounts']
+redeem_collection = db["redeem_codes"]
